da23a5b3adb0a26475afacf83f2697e9c0e87855
Christian Reis Fagundes Gomes <christianrfg@gmail.com>
2019-08-07 14:21:33 -0300

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-import setuptools
 from os import path
 
-# read the contents of your README file
+import setuptools
+
+# Read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -11,14 +12,14 @@ setuptools.setup(
     version='0.1',
     author="Christian Gomes",
     author_email="christianrfg@gmail.com",
-    description="Topic Modeling Quality Metrics",
+    description="Quality Metrics for Topic Modeling",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/christianrfg/tm_metrics",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-         "License :: MIT License",
-         "Operating System :: Ubuntu 18.04",
+        "License :: MIT License",
+        "Operating System :: Ubuntu 18.04",
     ],
 )
